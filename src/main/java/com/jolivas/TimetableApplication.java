922e2a728957cdbc7603a9749d549eecd2374105
Jesus Gallego Olivas <jesus.gallego.o@gmail.com>
2017-03-24 20:24:26 +0100

    prueba

--- src/main/java/com/jolivas/TimetableApplication.java
+++ src/main/java/com/jolivas/TimetableApplication.java
@@ -2,16 +2,9 @@ package com.jolivas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TimetableApplication extends SpringBootServletInitializer {
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder app){
-		return app.sources(TimetableApplication.class);
-	}
+public class TimetableApplication{
 
 	public static void main(String[] args) throws Exception{
 		SpringApplication.run(TimetableApplication.class, args);
